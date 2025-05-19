1b6272c100b7621e2708de69c7a232d62634f24a
hao_xiao_zi <xh483817@gmail.com>
2025-05-19 16:04:21 +0800

    day12·spring task入门案例

---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,22 @@
+package com.sky.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 34255
+ * Date: 2025-05-19
+ * Time: 15:56
+ */
+@Component
+@Slf4j
+public class MyTask {
+
+    @Scheduled(cron = "*/3 * * * * ?")
+    public void taskOne(){
+        log.info("定时任务，每3S执行一次");
+    }
+}
